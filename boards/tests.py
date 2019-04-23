f099e509d45436abcd4045fbbb85870674d8a871
hoyoung2176 <hoyoung2176@gamil.com>
2019-04-23 01:48:28 +0000

    02_modelform test

--- boards/tests.py
+++ boards/tests.py
@@ -12,12 +12,30 @@ class SettingsTest(TestCase):
         self.assertEqual(settings.LANGUAGE_CODE, 'ko-kr')
         self.assertEqual(settings.TIME_ZONE, 'Asia/Seoul')
         
-# 2. Model test
+# 2. Model test + ModelForm test
 class BoardModelTest(TestCase):
     def test_01_model(self):
         # board = Board.objects.create(title='test title', content='test content')
         board = Board.objects.create(title='test title', content='test content', user_id = 1)
         self.assertEqual(str(board), f'Board{board.pk}', msg="출력 값이 일치하지 않음")
+        
+    def test_02_boardform(self):
+        # given
+        data = {'title': '제목', 'content':'내용'}
+        # when then
+        self.assertEqual(BoardForm(data).is_valid(), True)
+        
+    def test_03_boardform_without_title(self):
+        # given
+        data = {'content':'내용'}
+        self.assertEqual(BoardForm(data).is_valid(), False)
+        
+    def test_04_boardform_without_content(self):
+        data = {'title':'제목'}
+        self.assertEqual(BoardForm(data).is_valid(), False)
+        
+        
+        
         
 # 3. View test
 class BoardViewTest(TestCase):
@@ -62,8 +80,12 @@ class BoardViewTest(TestCase):
             
             # then | 글이 작성되
             response = self.post('boards:create', data=data)
-            self.assertContains(response, '폼에 필수 항목 입니다.')
+            # self.assertContains(response, '폼에 필수 항목 입니다.')
+            self.assertContains(response, '')
+            # form.is_valid() 를 통과 하지 못해서 팅겨져 나옴.
             
+
+    
     
             
         
